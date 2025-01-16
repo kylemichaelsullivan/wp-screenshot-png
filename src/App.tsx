05a0cbd5f4278982ed48fa629292f3aec9f5590b
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-01-15 21:44:37 -0500

    FIX: Add .text-center

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Buttons from '@/components/Buttons';
 function App() {
 	return (
 		<div className='App flex flex-col gap-4'>
+			<h1 className='text-2xl font-bold text-center'>WordPress Screenshot Maker</h1>
 			<Input />
 			<Output />
 			<Buttons />
